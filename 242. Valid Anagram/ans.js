45ce5266365cd861a632501c5d44de4880aeaf65
Keeno Lee <keenoleecw@gmail.com>
2023-08-23 00:13:57 -0400

    updated: 242. Valid Anagram js ans

--- 242. Valid Anagram/ans.js
+++ 242. Valid Anagram/ans.js
@@ -17,3 +17,34 @@
 // s and t consist of lowercase English letters.
 
 // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const charCount = new Array(26).fill(0); // Assuming lowercase English letters
+
+  // Count occurrences of characters in s
+  for (let char of s) {
+    charCount[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+  }
+
+  // Decrement occurrences of characters in t
+  for (let char of t) {
+    charCount[char.charCodeAt(0) - "a".charCodeAt(0)]--;
+  }
+
+  // Check if all counts are zero
+  for (let count of charCount) {
+    if (count !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test cases
+console.log(isAnagram("anagram", "nagaram")); // Output: true
+console.log(isAnagram("rat", "car")); // Output: false
