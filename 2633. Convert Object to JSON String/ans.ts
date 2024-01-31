862837809e44e0e4af143b72217b2c72142664ed
Keeno Lee <keenoleecw@gmail.com>
2024-01-30 23:37:06 -0500

    updated: 2633. Convert Object to JSON String - ts

--- 2633. Convert Object to JSON String/ans.ts
+++ 2633. Convert Object to JSON String/ans.ts
@@ -37,3 +37,36 @@
 // 1 <= JSON.stringify(object).length <= 105
 // maxNestingLevel <= 1000
 // all strings contain only alphanumeric characters
+
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+function jsonStringify(object: any): string {
+  if (object === null) {
+    return "null";
+  }
+
+  if (Array.isArray(object)) {
+    const elements = object.map((element: any) => jsonStringify(element));
+    return `[${elements.join(",")}]`;
+  }
+
+  if (typeof object === "object") {
+    const keys = Object.keys(object);
+    const keyValuePairs = keys.map(
+      (key: string) => `"${key}":${jsonStringify(object[key])}`
+    );
+    return `{${keyValuePairs.join(",")}}`;
+  }
+
+  if (typeof object === "string") {
+    return `"${object}"`;
+  }
+
+  return String(object);
+}
