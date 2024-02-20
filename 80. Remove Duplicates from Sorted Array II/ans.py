1f24535e5e35fcde6e46bf5d07236dbe166e8bb4
Keeno Lee <keenoleecw@gmail.com>
2024-02-19 23:46:45 -0500

    solved: 80. Remove Duplicates from Sorted Array II - py

---
+++ 80. Remove Duplicates from Sorted Array II/ans.py
@@ -0,0 +1,92 @@
+# 80. Remove Duplicates from Sorted Array II
+# Medium
+# Topics
+# Companies
+# Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+# Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+# Return k after placing the final result in the first k slots of nums.
+
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+
+# int[] nums = [...]; // Input array
+# int[] expectedNums = [...]; // The expected answer with correct length
+
+# int k = removeDuplicates(nums); // Calls your implementation
+
+# assert k == expectedNums.length;
+# for (int i = 0; i < k; i++) {
+#     assert nums[i] == expectedNums[i];
+# }
+# If all assertions pass, then your solution will be accepted.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3]
+# Output: 5, nums = [1,1,2,2,3,_]
+# Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+# Example 2:
+
+# Input: nums = [0,0,1,1,1,1,2,3,3]
+# Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+# Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -104 <= nums[i] <= 104
+# nums is sorted in non-decreasing order.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        # Initialize the counter and the array index.
+        i, count = 1, 1
+        
+        # Start from the second element of the array and process
+        # elements one by one.
+        while i < len(nums):
+            
+            # If the current element is a duplicate, 
+            # increment the count.
+            if nums[i] == nums[i - 1]:
+                count += 1
+                
+                # If the count is more than 2, this is an
+                # unwanted duplicate element and hence we 
+                # remove it from the array.
+                if count > 2:
+                    nums.pop(i)
+                    
+                    # Note that we have to decrement the
+                    # array index value to keep it consistent
+                    # with the size of the array.
+                    i-= 1
+                
+            else:
+                
+                # Reset the count since we encountered a different element
+                # than the previous one
+                count = 1
+           
+            # Move on to the next element in the array
+            i += 1    
+                
+        return len(nums)
+    
+## Runtime 61ms (Beats 19.83%)
+## Memory 16.62 MB (Beats 60.84%)
