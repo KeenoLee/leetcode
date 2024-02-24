c91ba3f568d422d542a0247fdcec6db908f700e2
Keeno Lee <keenoleecw@gmail.com>
2024-02-23 19:09:25 -0500

    solved: 28. Find the Index of the First Occurrence in a String - py

---
+++ 28. Find the Index of the First Occurrence in a String/ans.py
@@ -0,0 +1,43 @@
+# 28. Find the Index of the First Occurrence in a String
+# Solved
+# Easy
+# Topics
+# Companies
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+# Example 2:
+
+# Input: haystack = "leetcode", needle = "leeto"
+# Output: -1
+# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+# Constraints:
+
+# 1 <= haystack.length, needle.length <= 104
+# haystack and needle consist of only lowercase English characters.
+
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m = len(needle)
+        n = len(haystack)
+
+        for window_start in range(n - m + 1):
+            for i in range(m):
+                if needle[i] != haystack[window_start + i]:
+                    break
+                if i == m - 1:
+                    return window_start
+
+        return -1
+         
