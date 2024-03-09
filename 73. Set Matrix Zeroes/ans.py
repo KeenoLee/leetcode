4774cf575c1c829e48499fe1bc2994b335f7bc94
Keeno Lee <keenoleecw@gmail.com>
2024-03-08 23:45:41 -0500

    sovled: 73. Set Matrix Zeroes - py

---
+++ 73. Set Matrix Zeroes/ans.py
@@ -0,0 +1,61 @@
+# 73. Set Matrix Zeroes
+# Medium
+# Topics
+# Companies
+# Hint
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+# You must do it in place.
+
+ 
+
+# Example 1:
+
+
+# Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# Output: [[1,0,1],[0,0,0],[1,0,1]]
+# Example 2:
+
+
+# Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+# Constraints:
+
+# m == matrix.length
+# n == matrix[0].length
+# 1 <= m, n <= 200
+# -231 <= matrix[i][j] <= 231 - 1
+ 
+
+# Follow up:
+
+# A straightforward solution using O(mn) space is probably a bad idea.
+# A simple improvement uses O(m + n) space, but still not the best solution.
+# Could you devise a constant space solution?
+
+# https://leetcode.com/problems/set-matrix-zeroes/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        R = len(matrix)
+        C = len(matrix[0])
+        rows, cols = set(), set()
+
+        # Essentially, we mark the rows and columns that are to be made zero
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        # Iterate over the array once again and using the rows and cols sets, update the elements
+        for i in range(R):
+            for j in range(C):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
