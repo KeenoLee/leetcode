038048398c7724397c5ad30588de44a3266dfbb1
Keeno Lee <keenoleecw@gmail.com>
2024-03-10 23:44:53 -0400

    solved: 290. Word Pattern - py

---
+++ 290. Word Pattern/ans.py
@@ -0,0 +1,56 @@
+# 290. Word Pattern
+# Solved
+# Easy
+# Topics
+# Companies
+# Given a pattern and a string s, find if s follows the same pattern.
+
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+# Example 1:
+
+# Input: pattern = "abba", s = "dog cat cat dog"
+# Output: true
+# Example 2:
+
+# Input: pattern = "abba", s = "dog cat cat fish"
+# Output: false
+# Example 3:
+
+# Input: pattern = "aaaa", s = "dog cat cat dog"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= pattern.length <= 300
+# pattern contains only lower-case English letters.
+# 1 <= s.length <= 3000
+# s contains only lowercase English letters and spaces ' '.
+# s does not contain any leading or trailing spaces.
+# All the words in s are separated by a single space.
+
+# https://leetcode.com/problems/word-pattern/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        map_char = {}
+        map_word = {}
+        
+        words = s.split(' ')
+        if len(words) != len(pattern):
+            return False
+        
+        for c, w in zip(pattern, words):
+            if c not in map_char:
+                if w in map_word:
+                    return False
+                else:
+                    map_char[c] = w
+                    map_word[w] = c
+            else:
+                if map_char[c] != w:
+                    return False
+        return True
